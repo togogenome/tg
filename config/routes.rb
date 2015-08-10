31fbcbc673471d2ab387dba4392d260f9f5d10b3
Naoki Nishiguchi <nnishiguchister@gmail.com>
2015-08-10 12:03:05 +0900

    Build outline on rails view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'root#show'
+
   namespace :api, defaults: {format: :json} do
     get '/sequence', as: :sequence_index, to: 'sequence#index'
     get '/sequence/search', as: :sequence_search, to: 'sequence#search'
